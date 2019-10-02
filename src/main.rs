41d832419571622012cecb370264f9edf979e8b1
Chris Froehlingsdorf <chris@5cf.de>
2019-10-02 18:53:07 +0200

    working on the queue db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::thread;
 use log::info;
 use std::time::Duration;
--- src/queue.rs
+++ src/queue.rs
@@ -1,9 +1,11 @@
 use rusqlite::types::ToSql;
 use rusqlite::{Connection, NO_PARAMS};
 use time::Timespec;
-use log::info;
+use log::{info, error};
 
 const EVENT_TYPE_ALERT: &str = "ALERT";
+const CURRENT_VERSION: i32 = 1;
+const VERSION_KEY: &str = "version";
 
 #[derive(Debug)]
 pub struct ILAgentItem {
@@ -71,19 +73,81 @@ impl Queue {
             NO_PARAMS,
         ).unwrap();
 
-        let mut stmt = self.conn.prepare("SELECT key, val, created_at FROM ilagent").unwrap();
-        let _keys = stmt
-            .query_map(NO_PARAMS, |row| Ok(ILAgentItem {
-                key: row.get(0).unwrap(),
-                val: row.get(1).unwrap(),
-                created_at: row.get(2).unwrap(),
-            })).unwrap();
+        let version = self.get_ilagent_value_as_number(VERSION_KEY);
+        match version {
+            None => {
+                info!("Database not bootstrapped yet.");
+                // TODO: boostrap
+                let result = self.create_ilagent_item(self.create_ilagent_item_instance(VERSION_KEY,CURRENT_VERSION.to_string().as_str()));
+                match result {
+                    Err(e) => panic!("Failed to bootstrap database: {:?}.", e),
+                    Ok(result_val) => info!("Bootstrapped database {}.", result_val),
+                };
+            },
+            Some(version_val) => {
+                info!("Database on version {}.", version_val);
+                // TODO: validate if version needs upgrade
+            },
+        };
 
-        info!("Database prepared.");
         ()
     }
 
-    pub fn create_incident(&self, api_key: &str, summary: &str) -> Result<IncidentQueueItem, &'static str> {
+    fn get_ilagent_value(&self, key: &str) -> Option<String> {
+
+        let mut stmt = self.conn.prepare("SELECT key, val, created_at FROM ilagent WHERE key = ?1").unwrap();
+        let items = stmt
+            .query_map(&[&key], |row| Ok(ILAgentItem {
+                key: row.get(0).unwrap(),
+                val: row.get(1).unwrap(),
+                created_at: row.get(2).unwrap(),
+            }));
+
+        match items {
+            Err(e) => {
+                error!("Failed to get ilagent value: {}; {:?}.", key, e);
+                None
+            },
+            Ok(mut item_values) => {
+                match item_values.next() {
+                    None => None,
+                    Some(val) => Some(val.unwrap().val),
+                }
+            },
+        }
+    }
+
+    fn get_ilagent_value_as_number(&self, key: &str) -> Option<i32> {
+        let row_val = self.get_ilagent_value(key);
+        match row_val {
+            None => None,
+            Some(val) => match val.parse::<i32>() {
+                Err(e) => panic!("Error during parsing of the ilagent db version {:?}.", e),
+                Ok(int_val) => Some(int_val)
+            }
+        }
+    }
+
+    fn create_ilagent_item_instance(&self, key: &str, val: &str) -> ILAgentItem {
+
+        let incident = ILAgentItem {
+            key: key.to_string(),
+            val: val.to_string(),
+            created_at: time::get_time(),
+        };
+
+        incident
+    }
+
+    fn create_ilagent_item(&self, item: ILAgentItem) -> Result<usize,  rusqlite::Error> {
+        self.conn.execute(
+            "INSERT INTO ilagent (key, val, created_at)
+                          VALUES (?1, ?2, ?3)",
+            &[&item.key as &dyn ToSql, &item.val, &item.created_at],
+        )
+    }
+
+    pub fn create_incident_instance(&self, api_key: &str, summary: &str) -> IncidentQueueItem {
 
         let incident = IncidentQueueItem {
             id: 0,
@@ -94,36 +158,38 @@ impl Queue {
             created_at: time::get_time(),
         };
 
-        Ok(incident)
+        incident
     }
 
-    pub fn store_incident(&self, incident: IncidentQueueItem) -> Result<usize,  rusqlite::Error> {
+    pub fn get_incidents(&self, limit: i32) -> Vec<Result<IncidentQueueItem,  rusqlite::Error>> {
+
+        let mut stmt = self.conn.prepare("SELECT id, api_key, event_type, incident_key, summary, created_at FROM incident_items LIMIT ?1").unwrap();
+        let items = stmt
+            .query_map(&[&limit], |row| Ok(IncidentQueueItem {
+                id: row.get(0).unwrap(),
+                api_key: row.get(1).unwrap(),
+                event_type: row.get(2).unwrap(),
+                incident_key: row.get(3).unwrap(),
+                summary: row.get(4).unwrap(),
+                created_at: row.get(5).unwrap(),
+            })).unwrap();
+
+        items.collect::<Vec<Result<IncidentQueueItem,  rusqlite::Error>>>()
+    }
+
+    pub fn create_incident(&self, incident: IncidentQueueItem) -> Result<usize,  rusqlite::Error> {
          self.conn.execute(
-            "INSERT INTO person (api_key, event_type, incident_key, summary, created_at)
+            "INSERT INTO incident_items (api_key, event_type, incident_key, summary, created_at)
                   VALUES (?1, ?2, ?3, ?4, ?5)",
             &[&incident.api_key as &dyn ToSql, &incident.event_type, &incident.incident_key,
                 &incident.summary, &incident.created_at],
         )
     }
 
-    pub fn test(&self) -> () {
-
-        /*
-        let mut stmt = self.conn
-            .prepare("SELECT id, name, time_created, data FROM person").unwrap();
-
-        let person_iter = stmt
-            .query_map(NO_PARAMS, |row| Ok(Person {
-                id: row.get(0).unwrap(),
-                name: row.get(1).unwrap(),
-                time_created: row.get(2).unwrap(),
-                data: row.get(3).unwrap(),
-            })).unwrap();
-
-        for person in person_iter {
-            info!("Found person {:?}", person.unwrap());
-        } */
-
-        ()
+    pub fn delete_incident(&self, incident: IncidentQueueItem) -> Result<usize,  rusqlite::Error> {
+        self.conn.execute(
+            "DELETE FROM incident_items WHERE id = ?1",
+            &[&incident.id],
+        )
     }
 }
